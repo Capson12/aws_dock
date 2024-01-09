1941074071249efe76345944649a8bc014b2cb0e
Rual Oguh <rualoguh@gmail.com>
2024-01-09 18:49:34 +0000

    add subnet-3

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,8 +25,10 @@ resource "aws_vpc" "vpc1" {
 
 resource "aws_subnet" "smtx_sub1" {
   vpc_id = aws_vpc.vpc1.id
-  cidr_block = "10.12.1.0/24"
+  cidr_block = "10.0.1.0/24"
   availability_zone = "eu-west-2c"
+  
+  
 
 
   tags = {
