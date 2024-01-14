9e5aae01b4a43c167d77d83ff151f2c9c47e7078
capson12 <rualoguh@gmail.com>
2024-01-14 18:56:26 +0000

    rectify mistakes in Nic

--- infrastructure/vm.tf
+++ infrastructure/vm.tf
@@ -4,22 +4,28 @@ resource "aws_instance" "aws_ami_image" {
     instance_type = "t2.micro"
     subnet_id = aws_subnet.smtx_sub1.id
     
-    network_interface {
-
-      subnet_id = smtx_sub1.id
-
-    }
-    
     tags = {
       name = "nodemachine"
     }
   
 }
 
+
+resource "aws_network_interface" "smtx_nic" {
+  subnet_id = aws_subnet.smtx_sub1.id
+  
+
+  attachment {
+    instance = aws_instance.aws_ami_image.id
+    device_index = 1
+  }
+  
+}
+
+
 resource "aws_eip_association" "eip1" {
   instance_id = aws_instance.aws_ami_image.id
-  domain = "vpc"
-  network_interface_id = aws_instance.aws_ami_image.id
+  network_interface_id = aws_network_interface.smtx_nic.id
   
 
   
