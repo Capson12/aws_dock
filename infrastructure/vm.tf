8cf30da47c7487ccf7793e70de657d0ba88d81eb
capson12 <rualoguh@gmail.com>
2024-01-16 14:10:05 +0000

    add nw rules

--- infrastructure/vm.tf
+++ infrastructure/vm.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "aws_ami_image" {
     ami = "ami-0500f74cc2b89fb6b"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.smtx_sub1.id
+    key_name = aws_key_pair.smtx_vm_key.id
+    
     
     tags = {
       name = "nodemachine"
@@ -28,3 +30,9 @@ resource "aws_eip" "smtx_eip" {
   instance = aws_instance.aws_ami_image.id
   domain = "vpc"
 }
+
+# resource "aws_key_pair" "smtx_vm_key" {
+  
+  
+  
+# }
